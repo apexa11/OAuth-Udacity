fafe45b72cfd174a22060a241205448140497767
apexa11 <apexachoxi7@gmail.com>
2017-07-05 12:23:55 -0400

    update

--- database_setup.py
+++ database_setup.py
@@ -2,12 +2,12 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
- 
+
 Base = declarative_base()
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-   
+
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
@@ -18,7 +18,7 @@ class Restaurant(Base):
            'name'         : self.name,
            'id'           : self.id,
        }
- 
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
 
@@ -46,6 +46,6 @@ class MenuItem(Base):
 
 
 engine = create_engine('sqlite:///restaurantmenu.db')
- 
+
 
 Base.metadata.create_all(engine)
