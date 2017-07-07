4500cee4bbffa8d7d8b651d1fbecb2cd46d488e4
apexa11 <apexachoxi7@gmail.com>
2017-07-06 22:19:54 -0400

    user model update

--- database_setup.py
+++ database_setup.py
@@ -5,11 +5,23 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+  __tablename__ = 'user'
+
+  id = Column(Integer, primary_key=True)
+  name = Column(String(250), nullable=False)
+  email = Column(String(250), nullable=False)
+  picture = Column(String(250))
+
+
+
 class Restaurant(Base):
     __tablename__ = 'restaurant'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    user_id = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
@@ -29,7 +41,9 @@ class MenuItem(Base):
     price = Column(String(8))
     course = Column(String(250))
     restaurant_id = Column(Integer,ForeignKey('restaurant.id'))
+    user_id = Column(Integer,ForeignKey('user.id'))
     restaurant = relationship(Restaurant)
+    user = relationship(User)
 
 
     @property
@@ -45,7 +59,7 @@ class MenuItem(Base):
 
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 
 
 Base.metadata.create_all(engine)
