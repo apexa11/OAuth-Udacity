4caab6b0463193dd8d67ad0cd12b8ee53f76845f
apexa11 <apexachoxi7@gmail.com>
2017-07-11 22:27:32 -0400

    fixes

--- project.py
+++ project.py
@@ -269,10 +269,6 @@ def fbdisconnect():
     return "you have been logged out"
 
 
-
-
-
-
 #JSON APIs to view Restaurant Information
 @app.route('/restaurant/<int:restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
@@ -431,6 +427,28 @@ def deleteMenuItem(restaurant_id,menu_id):
       return redirect(url_for('showMenu', restaurant_id = restaurant_id))
   else:
       return render_template('deleteMenuItem.html', item = itemToDelete)
+
+# Disconnect based on provider
+@app.route('/disconnect')
+def disconnect():
+    if 'provider' in login_session:
+        if login_session['provider'] == 'google':
+            gdisconnect()
+            del login_session['gplus_id']
+            del login_session['credentials']
+        if login_session['provider'] == 'facebook':
+            fbdisconnect()
+            del login_session['facebook_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        del login_session['provider']
+        flash("You have successfully been logged out.")
+        return redirect(url_for('showRestaurants'))
+    else:
+        flash("You were not logged in")
+        return redirect(url_for('showRestaurants'))
 
 
 
